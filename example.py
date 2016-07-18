39064cd292bbd6343514080de15290c184700f26
Matthias <MMesch@users.noreply.github.com>
2016-07-19 00:01:25 +0200

    with fourier spectrum

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ def main():
     array_width = 20
     l0 = 1
 
-    npts1d = 10
+    npts1d = 20
     npts = npts1d**2
     x = np.linspace(-array_width/2, array_width/2, npts1d)
     y = np.linspace(-array_width/2, array_width/2, npts1d)
@@ -20,18 +20,20 @@ def main():
 
     distances = np.sqrt((xs[None, :] - xs[:, None])**2 +
                         (ys[:, None] - ys[None, :])**2)
-    # covariance = jn(0, 2. * np.pi * distances)
-    covariance = np.exp(-distances**2 / 5.)
+    covariance = jn(0, distances)
 
-    covariancex = np.exp(-(x[None, :] - x[:, None])**2/5.)
-    covariancey = np.exp(-(y[None, :] - y[:, None])**2/5.)
-    covariance_combined = np.einsum('ij, kl -> ikjl', covariancex, covariancey)
-    covariance_combined = covariance_combined.reshape(npts, npts)
+    # uncomment the following for separable Gaussian cv matrix:
+    # covariance = np.exp(-distances**2 / 5.)
 
-    fig, (col1, col2, col3) = plt.subplots(1, 3)
-    col1.imshow(covariancex)
-    col2.imshow(covariancey)
-    col3.imshow(covariance_combined, cmap='viridis')
+    # covariancex = np.exp(-(x[None, :] - x[:, None])**2/5.)
+    # covariancey = np.exp(-(y[None, :] - y[:, None])**2/5.)
+    # covariance_combined = np.einsum('ij, kl -> ikjl', covariancex, covariancey)
+    # covariance_combined = covariance_combined.reshape(npts, npts)
+
+    # fig, (col1, col2, col3) = plt.subplots(1, 3)
+    # col1.imshow(covariancex)
+    # col2.imshow(covariancey)
+    # col3.imshow(covariance_combined, cmap='viridis')
 
     evals, evecs = np.linalg.eigh(covariance)
 
@@ -45,35 +47,23 @@ def main():
     col2.plot(np.linspace(0., 1., npts), evals/evals.max())
     col2.set(xlabel='eigenvector', ylabel='eigenvalue or spatial coordinate',
              xlim=(0, 1), ylim=(0, 1), title='eigenvectors and values')
+
+    # compute Fourier transform of covariance function:
+    covariance = jn(0, np.sqrt(xgrid**2 + ygrid**2))
+    fig, (col1, col2, col3) = plt.subplots(1, 3)
+    norm = plt.Normalize(-covariance.max(), covariance.max())
+    col1.imshow(covariance.T, origin='lower', cmap='RdBu', aspect='auto')
+    covariance_fft = np.fft.ifft2(covariance)
+    covariance_fft = np.fft.fftshift(covariance, axes=(0))
+    power = np.abs(covariance_fft)
+    col2.imshow(power, origin='lower', cmap='viridis', aspect='auto')
+
+    isorted = np.argsort(power.flatten())
+    npts_all = len(isorted)
+    power_sorted = power.flatten()[isorted] / power.max()
+    col3.plot(np.linspace(0., 1., npts_all), power_sorted)
+
     plt.show()
-
-    # Computed from Hankel (Fourier-Bessel) transform
-    rho = np.sqrt(xgrid**2 + ygrid**2)
-    kx = np.linspace(-np.pi/dx, np.pi/dx)
-    kxgrid, kygrid = np.meshgrid(kx, kx, indexing='ij')
-    kabs = np.sqrt(kxgrid**2 + kygrid**2)
-
-    # n = 9
-    # alpha_0 = besselzero(n, nx*nx, 1)
-    #
-    # 
-    #c = zeros(1, nx*nx) ;
-    #
-    # 
-    #drho = diff(rho) ;
-    #rho = rho[1:-1] ;
-    #for ii = 1:nx*nx
-    #    cdrho = k0^2 * (besselj(0, rho*k0)) .* besselj(n, alpha_0(ii)*k0*rho/L) .* rho .* drho ;
-    #    c(ii) = sum(cdrho) ;
-    #    c(ii) = 2*c(ii)/(L^2*besselj(n+1, alpha_0(ii))^2);
-    #end
-    #phi = c ;
-    #
-    #figure(2)
-    #clf
-    #plot(lambdas, '.-')
-    #plot(phi, '.-')
-    #box on
 
 
 if __name__ == "__main__":
